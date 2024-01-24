ec743083dda71b8ba9957bdd6263acb37a939ecd
ThatGhost <stargamer.me@gmail.com>
2024-01-24 16:56:28 +0100

    added activity feed for profile

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ActivityChartComponent } from '../activity-chart/activity-chart.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
-  imports: [ActivityChartComponent]
+  imports: [ActivityChartComponent, CommonModule]
 })
 export class ProfileComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   profileId = 0;
+
+  activities: { title: string, image: string }[] = [
+    {title: "Updated docs", image: "/assets/bell-icon.png"},
+    {title: "Awnsered question", image: "/assets/news-icon.png"},
+    {title: "New bio!", image: "/assets/profile-icon.png"},
+    {title: "Updated docs", image: "/assets/bell-icon.png"}]
 
   constructor() {
     this.profileId = Number(this.route.snapshot.params['id']);
